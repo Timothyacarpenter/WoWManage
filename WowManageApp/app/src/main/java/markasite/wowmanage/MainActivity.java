9cf06b2e518d661d581a811153f2995e061172d9
DESKTOP-KKI7E1O\Matthew <matt.l@markasite.com>
2016-12-14 15:45:22 +1100

    added a User Account class to stone user imformation.

--- WowManageApp/app/src/main/java/markasite/wowmanage/MainActivity.java
+++ WowManageApp/app/src/main/java/markasite/wowmanage/MainActivity.java
@@ -3,6 +3,10 @@ package markasite.wowmanage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**so I guess the first thing we want users to do is add account details that will be saved
+ * for now we will have an internal account the users simply puts in their battletag - but later on we can add battlenet support
+ * i saw some information on this but this should do for now*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ WowManageApp/app/src/main/java/markasite/wowmanage/Model/UserAccount.java
@@ -0,0 +1,19 @@
+package markasite.wowmanage.Model;
+
+/**
+ * Created by Matthew on 14/12/2016.
+ * A user account that will contain all the relavent details - for now only Contains the Battletag
+ */
+
+public class UserAccount {
+
+
+
+    private String battleTag;
+
+    public UserAccount(String battleTag){
+        this.battleTag = battleTag;
+    }
+
+
+}
