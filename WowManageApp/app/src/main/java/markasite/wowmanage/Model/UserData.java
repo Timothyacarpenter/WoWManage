0cb276a3c7557d43b6f8a41588698c5e8e943688
DESKTOP-KKI7E1O\Matthew <matt.l@markasite.com>
2016-12-14 15:59:43 +1100

    Added a user data that will work simmilar to the event model

---
+++ WowManageApp/app/src/main/java/markasite/wowmanage/Model/UserData.java
@@ -0,0 +1,11 @@
+package markasite.wowmanage.Model;
+
+/**
+ * Created by Matthew on 14/12/2016.
+ * going to be used like the event model with a singlton instance with all the valid battletags that have been used.
+ * if userdata is empty on starting the app the app should prompt the player to input their Battletag
+ */
+
+public class UserData {
+
+}
